33931248abbff80c6d843e3054fa097600c129ff
ProgramSalamander <464243149@qq.com>
2017-10-13 14:52:32 +0800

    Refactor: homepage add two new background images optimize position

--- index.php
+++ index.php
@@ -9,81 +9,69 @@
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="js/uikit.min.js"></script>
         <script src="js/uikit-icons.min.js"></script>
+        <script src="js/cookie.js"></script>
     </head>
-    <body class="uk-background-cover uk-light">
+    <body class="uk-background-cover uk-position-relative">
         <div class="uk-position-center">
-            <h1 class="uk-text-bold uk-margin-medium-bottom uk-text-center uk-flex-middle uk-animation-slide-top">嗨！爱拍吗</h1>
-            <!--            <ul>-->
-            <!--                <li>-->
-            <!--                    <button id="signInBtn" class="uk-button-primary uk-animation-fade signBtn">登录</button>-->
-            <!--                </li>-->
-            <!--                <li>-->
-            <!--                    <button id="signUpBtn" class="uk-button-primary uk-animation-fade signBtn">注册</button>-->
-            <!--                </li>-->
-            <!--            </ul>-->
+            <h1 class="uk-heading-primary uk-text-bold uk-margin-medium-bottom uk-text-center uk-flex-middle uk-animation-slide-top"> 嗨！爱拍吗</h1>
+            <div class="uk-width-auto">
+                <div class="uk-position-bottom-center uk-position-relative" style="text-align: center">
+                    <button class="uk-button-large uk-button-secondary"> 加入约拍!</button>
+                </div>
+            </div>
         </div>
-        <div class="uk-position-bottom-center uk-position-medium">
+        <div class="uk-light uk-position-bottom-center uk-position-medium">
             <ul class="uk-dotnav">
-                <li class="uk-active"><a></a></li>
+                <li class="uk-active"><a href=""></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
                 <li><a></a></li>
                 <li><a></a></li>
                 <li><a></a></li>
             </ul>
         </div>
 
-        <a id="previousArrow" uk-icon="icon:triangle-up; ratio:3"></a>
-
-        <div class="uk-position-center signDiv" id="signInDiv">
-            <h2>登录</h2>
-            <hr/>
-            <form>
-                <ul>
-                    <li><label for="signInUsername">用户名</label><input id="signInUsername" type="text"/></li>
-                    <li><label for="signInPassword">密码</label><input id="signInPassword" type="password"/></li>
-                    <li>
-                        <button class="signBtn" id="signInSubmit" type="button">提交</button>
-                    </li>
-                </ul>
-            </form>
-        </div>
-
-
-        <div class="uk-position-center signDiv" id="signUpDiv">
-            <h2>注册</h2>
-            <hr/>
-            <form>
-                <ul>
-                    <li><label for="signUpUsername">用户名</label><input id="signUpUsername" type="text"/></li>
-                    <li id="likePart"><label>喜欢</label><label><input name="signUpRole" id="signUpRoleActive" type="radio"/>拍人</label><label><input
-                                    name="signUpRole" id="signUpRolePassive" type="radio"/>被拍</label>
-                    </li>
-                    <li><label for="signUpPassword">密码</label><input id="signUpPassword" type="password"/></li>
-                    <li><label for="signUpEnsurePassword">确认密码</label><input id="signUpEnsurePassword" type="password"/></li>
-                    <li>
-                        <button class="signBtn" id="signUpSubmit" type="button">提交</button>
-                    </li>
-                </ul>
-            </form>
-        </div>
-
     </body>
-    <script src="js/cookie.js"></script>
     <script>
         $('document').ready(function () {
-            let imgPathArr = ['../imgs/index/bg1.jpg', '../imgs/index/bg2.jpg', '../imgs/index/bg3.jpg', '../imgs/index/bg4.jpg'];
-            let dotNavArr = $('.uk-dotnav li');
-            let counter = 1;
-            setInterval(function () {
+            let imgPathArr = ['../imgs/index/bg1.jpg',
+                '../imgs/index/bg2.jpg',
+                '../imgs/index/bg3.jpg',
+                '../imgs/index/bg4.jpg',
+                '../imgs/index/bg5.jpg',
+                '../imgs/index/bg6.jpg'];
+            let liArr = $('.uk-dotnav li');
+
+            let counter = 0;
+
+            let timer = setInterval(changeBgImg, 5000);
+
+            $.each($('.uk-dotnav li a'), function (index, element) {
+                $(this).bind('click', function () {
+                    clearInterval(timer);
+                    changeBgImg(index);
+                    timer = setInterval(changeBgImg, 5000);
+                });
+            });
+
+            function changeBgImg(index) {
+                liArr[counter].className = '';
+                if(index) {
+                    counter = index;
+                }
+                else{
+                    if (counter >= imgPathArr.length - 1) {
+                        counter = 0;
+                    }
+                    else {
+                        counter++;
+                    }
+                }
                 $('body').css('background-image', `url(${imgPathArr[counter]})`);
-                dotNavArr[counter - 1 < 0 ? dotNavArr.length - 1 : counter - 1].className = '';
-                dotNavArr[counter].className = 'uk-active';
-                if (counter === imgPathArr.length - 1) {
-                    counter = 0;
-                }
-                else {
-                    counter++;
-                }
-            }, 5000);
+                liArr[counter].className = 'uk-active';
+
+            }
+
 
             $('#signInSubmit').click(function () {
                 $.ajax({
