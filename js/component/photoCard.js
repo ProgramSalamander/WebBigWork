8db5ecf84aa6d481ba4e0eb5e8a97a986a81f7a0
ProgramSalamander <464243149@qq.com>
2017-11-03 22:50:51 +0800

    基本完成四处逛逛，新增约拍页

---
+++ js/component/photoCard.js
@@ -0,0 +1,29 @@
+class PhotoCard {
+
+    constructor(data){
+        this.data = data;
+    }
+
+    render(){
+        return $(`<div class="uk-card uk-card-hover uk-card-default uk-margin-bottom">\
+                        <div>\
+                            <div class="uk-inline-clip uk-transition-toggle">\
+                                <a href=""><img class="uk-transition-scale-up uk-transition-opaque"\
+                                                                     src="${this.data.photoUrl}"></a>\
+                            </div>\
+                        </div>\
+                        <div class="uk-grid-small uk-padding-small" uk-grid>\
+                            <div class="uk-width-auto">\
+                                <span class="uk-icon" uk-icon="icon: user"></span><a class="uk-text-small" title="去ta的主页" uk-tooltip>${this.data.photoAuthor}</a>\
+                            </div>\
+                            <div class="uk-width-expand uk-text-center">\
+                                <span class="uk-label label-scenery">${this.data.photoLabel}</span>\
+                            </div>\
+                            <div class="uk-width-auto">\
+                                <a href="" class="uk-icon-link" uk-icon="icon:heart" title="喜欢" uk-tooltip></a><a\
+                                    href="" class="uk-margin-left uk-icon-link" uk-icon="icon:commenting" title="评论" uk-tooltip></a>\
+                            </div>\
+                        </div>\
+                    </div>`);
+    }
+}
