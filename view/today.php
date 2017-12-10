46995d41b79f5cf130e5c49058d444df29db713c
ProgramSalamander <464243149@qq.com>
2017-12-11 02:16:46 +0800

    更新首页今日明星

--- view/today.php
+++ view/today.php
@@ -6,6 +6,22 @@ checkSignIn();
 
 $headPicUrl = getHeadPicURL($_SESSION['user_info']['head_pic_url']);
 $myUsername = $_COOKIE['username'];
+
+$todayHotPhotos = array();
+try {
+    $db = getDB();
+    $ret = $db->query("SELECT p.photo_url,a.album_name,u.username,u.nick_name,l.label_eng_name,l.label_chi_name FROM photo AS p, album AS a, user AS u, label AS l WHERE p.album_id = a.album_id AND a.user_id = u.user_id AND p.label_id = l.label_id ORDER BY p.photo_likes, p.photo_comments DESC LIMIT 10");
+    while ($row = $ret->fetchArray()) {
+        $photo = array();
+        $photo['photoUrl'] = getPhotoURL($row['username'], $row['album_name'], $row['photo_url']);
+        $photo['author'] = $row['nick_name'];
+        $photo['label_chi'] = $row['label_chi_name'];
+        $photo['label_eng'] = $row['label_eng_name'];
+        array_push($todayHotPhotos, $photo);
+    }
+} catch (Exception $e) {
+    header('location: error.php');
+}
 ?>
 <html lang="zh">
     <head>
@@ -86,10 +102,9 @@ $myUsername = $_COOKIE['username'];
 
                     $.each(hotData, function (index, item) {
                         let photoCard = new PhotoCard(item, $('#column1').width());
-                        getShortestColumn($('#column1'),$('#column2'),$('#column3'),$('#column4')).append(photoCard.render());
+                        getShortestColumn($('#column1'), $('#column2'), $('#column3'), $('#column4')).append(photoCard.render());
                     });
                 }
-
 
 
             });
@@ -118,7 +133,7 @@ $myUsername = $_COOKIE['username'];
                         <li><a href="friendsNews.php">朋友圈<span class="uk-badge">8</span></a></li>
                         <li>
                             <a id="myHeadPic" href="homepage.php?username=<?php echo $myUsername ?>">
-                            <script>
+                                <script>
                                     $('#myHeadPic').prepend(new MyHeadPic('<?php echo $headPicUrl?>', 50).render());
                                 </script>
                             </a>
@@ -147,84 +162,40 @@ $myUsername = $_COOKIE['username'];
                 </ul>
             </section>
             <hr class="uk-divider-icon"/>
-            <section id="todayStar" class="uk-container uk-padding uk-padding-remove-top">
+            <section id="todayStar" class="uk-padding uk-padding-remove-top uk-position-relative">
                 <h2 class="uk-position-relative uk-position-top-center">今日明星</h2>
-                <div class="uk-grid uk-grid-divider uk-padding uk-padding-remove-top uk-padding-remove-bottom uk-padding-remove-left uk-text-center"
-                     uk-grid>
-                    <div class="uk-grid uk-width-expand uk-child-width-1-3 uk-margin-right" uk-grid>
-                        <div class="">
-                            <div class="uk-position-relative uk-position-center">
-                                <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                    <img class="uk-transition-scale-up uk-transition-opaque top3-head-pic" src="../imgs/index/bg4.jpg"/>
-                                </div>
-                                <p>徐杨晨</p>
-                                <p>今日获得喜欢总数：<span class="uk-badge like-badge">1024</span></p>
-                                <img class="top3-icon" src="../imgs/today/star_first.png"/>
+                <div class="uk-padding uk-grid uk-child-width-1-3" uk-grid>
+                    <div id="second">
+                        <div class="uk-position-relative uk-position-center uk-text-center">
+                            <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
+                                <img class="uk-transition-scale-up uk-transition-opaque top3-head-pic" src="../imgs/index/bg4.jpg"/>
                             </div>
-                        </div>
-                        <div class="">
-                            <div class="uk-position-relative uk-position-center">
-                                <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                    <img class="uk-transition-scale-up uk-transition-opaque top3-head-pic" src="../imgs/index/bg4.jpg"/>
-                                </div>
-                                <p>徐梓航</p>
-                                <p>今日获得喜欢总数：<span class="uk-badge like-badge">512</span></p>
-                                <img class="top3-icon" src="../imgs/today/star_second.png"/>
-                            </div>
-                        </div>
-                        <div class="">
-                            <div class="uk-position-relative uk-position-center">
-                                <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                    <img class="uk-transition-scale-up uk-transition-opaque top3-head-pic" src="../imgs/index/bg4.jpg"/>
-                                </div>
-                                <p>薛恺丰</p>
-                                <p>今日获得喜欢总数：<span class="uk-badge like-badge">256</span></p>
-                                <img class="top3-icon" src="../imgs/today/star_third.png"/>
-
-                            </div>
+                            <p>徐梓航</p>
+                            <p>今日获得喜欢总数：<span class="uk-badge like-badge">512</span></p>
+                            <img class="top3-icon" src="../imgs/today/star_second.png"/>
                         </div>
                     </div>
-                    <div class="uk-width-1-3 uk-child-width-1-2" uk-grid>
-                        <div>
+                    <div id="first">
+                        <div class="uk-position-relative uk-position-center uk-text-center">
                             <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                <img class="uk-transition-scale-up uk-transition-opaque top10-head-pic" src="../imgs/index/bg4.jpg"/>
+                                <img class="uk-transition-scale-up uk-transition-opaque top3-head-pic" src="../imgs/index/bg4.jpg"/>
                             </div>
-                            <p>鄢一德 <span class="uk-badge like-badge">128</span></p>
+                            <p>徐杨晨</p>
+                            <p>今日获得喜欢总数：<span class="uk-badge like-badge">1024</span></p>
+                            <img class="top3-icon" src="../imgs/today/star_first.png"/>
                         </div>
-                        <div>
-                            <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                <img class="uk-transition-scale-up uk-transition-opaque top10-head-pic" src="../imgs/index/bg4.jpg"/>
-                            </div>
-                            <p>鄢二德 <span class="uk-badge like-badge">128</span></p>
-                        </div>
-                        <div>
-                            <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                <img class="uk-transition-scale-up uk-transition-opaque top10-head-pic" src="../imgs/index/bg4.jpg"/>
-                            </div>
-                            <p>鄢三德 <span class="uk-badge like-badge">128</span></p>
-                        </div>
-                        <div>
-                            <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                <img class="uk-transition-scale-up uk-transition-opaque top10-head-pic" src="../imgs/index/bg4.jpg"/>
-                            </div>
-                            <p>鄢四德 <span class="uk-badge like-badge">128</span></p>
-                        </div>
-                        <div>
-                            <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                <img class="uk-transition-scale-up uk-transition-opaque top10-head-pic" src="../imgs/index/bg4.jpg"/>
-                            </div>
-                            <p>鄢五德 <span class="uk-badge like-badge">128</span></p>
-                        </div>
-                        <div>
-                            <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
-                                <img class="uk-transition-scale-up uk-transition-opaque top10-head-pic" src="../imgs/index/bg4.jpg"/>
-                            </div>
-                            <p>鄢六德 <span class="uk-badge like-badge">128</span></p>
-                        </div>
-
                     </div>
+                    <div id="third">
+                        <div class="uk-position-relative uk-position-center uk-text-center">
+                            <div class="uk-border-circle uk-inline-clip uk-transition-toggle" title="去Ta的主页" uk-tooltip>
+                                <img class="uk-transition-scale-up uk-transition-opaque top3-head-pic" src="../imgs/index/bg4.jpg"/>
+                            </div>
+                            <p>薛恺丰</p>
+                            <p>今日获得喜欢总数：<span class="uk-badge like-badge">256</span></p>
+                            <img class="top3-icon" src="../imgs/today/star_third.png"/>
 
-
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
